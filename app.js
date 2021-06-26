6bbd48c99ef05a35edda22399341563acfa92697
sayiO <sayionias@gmail.com>
2021-06-26 23:17:39 +0200

    modified all the files and added image folder

---
+++ app.js
@@ -0,0 +1,37 @@
+const quizData = [{
+        question: 'How old is man utd?'
+        a: '100 years',
+        b: '95 years',
+        c: '20 years',
+        d: '35 years'
+    }, {
+        question: 'Who is the all time goalscorer at Man Utd?',
+        a: 'Rio Ferdinand',
+        b: 'Ruud van Nisteroy',
+        c: 'Wayne Rooney',
+        d: 'Bobby Charlton'
+
+    },
+    {
+        question: "What capacity is Old Trafford Stadium?",
+        a: '68 000',
+        b: '45 000',
+        c: '94 000',
+        d: '75 000'
+    },
+    {
+        question: 'Who was the longest serving manager for Manchester United?'
+        a: 'Sir Alex Ferguson',
+        b: 'Jose Mourhino',
+        c: 'David Moyes',
+        d: 'Ole Gunner Solskaer'
+
+    },
+    {
+        question: 'How many champions league did Man Utd win?'
+        a: '5',
+        b: '2',
+        c: '3',
+        d: '4'
+    }
+]
